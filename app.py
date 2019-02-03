7f4620ea720345802812a8938adc305f5210468d
Louis de Thanhoffer de Volcsey <ldethanhoffer@me.com>
2019-02-03 16:59:50 -0500

    add kNN

--- app.py
+++ app.py
@@ -225,4 +225,4 @@ def recommend():
     return 'these are your recommendations'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='useum-muse.herokuapp.com', port=port)
